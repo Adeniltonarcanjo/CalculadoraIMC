2ca3d8c6a786238b7b70182087eeafc952a11022
Adeniltonarcanjo <adeniltonmorais@gmail.com>
2020-08-19 16:17:48 -0300

    aula 02 parte 02

--- app/src/main/java/adeniltonmorais/android/calculadoraimc/MainActivity.kt
+++ app/src/main/java/adeniltonmorais/android/calculadoraimc/MainActivity.kt
@@ -1,11 +1,20 @@
 package adeniltonmorais.android.calculadoraimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bttonNcd = findViewById<Button>(R.id.button_ncd)
+        button_ncd.setOnClickListener{
+            val abrirNcd= Intent(this, NecessidadeCaloricaActivity::class.java)
+            startActivity(abrirNcd)
+        }
     }
 }
